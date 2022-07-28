f4dd4166b8adb70ef1178b2a4fb7722d5a5a6591
20000896 <jessica_liu@books.com.tw>
2022-07-28 13:55:45 +0800

    try - set room

--- sample/02-gateways/src/events/events.gateway.ts
+++ sample/02-gateways/src/events/events.gateway.ts
@@ -4,27 +4,45 @@ import {
   WebSocketGateway,
   WebSocketServer,
   WsResponse,
+  OnGatewayConnection,
+  ConnectedSocket
 } from '@nestjs/websockets';
 import { from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
   cors: {
     origin: '*',
   },
 })
-export class EventsGateway {
+export class EventsGateway implements OnGatewayConnection {
   @WebSocketServer()
   server: Server;
 
-  @SubscribeMessage('events')
-  findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
-    return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
+  handleConnection(@ConnectedSocket() socket: Socket, ...args: any[]) {
+      console.log('someone is conntecting', socket.broadcast.allSockets());
+      socket.emit('events', {data: 'is connecting'});
+      
   }
 
-  @SubscribeMessage('identity')
-  async identity(@MessageBody() data: number): Promise<number> {
-    return data;
+  @SubscribeMessage('events')
+  findAll(@MessageBody("me") me: string): Observable<WsResponse<string>> {
+    console.log(me);
+    return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: `me${item}`})));
+  }
+
+
+  @SubscribeMessage('joinroom')
+  joinRoom(@ConnectedSocket() client: Socket, @MessageBody('room') roomId?: string){
+
+    let _socketRoomId = roomId ? roomId : 'aRandomRoomId';
+    client.join(_socketRoomId);
+    
+    let _castMsg = `${client.id} join room: ${_socketRoomId}`;
+    client.to(_socketRoomId).emit('joinroom', _castMsg);
+
+    console.log(client.rooms);
+    return _castMsg;
   }
 }
